ea1f5683666dee9594a8123734ff4d4d498d53a5
Dustin Myers <dstnmyrs99@yahoo.com>
2021-01-28 16:45:58 -0600

    Updates

---
+++ precache-manifest.561832b4770955700e578cb3844d627e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a1c58a0b4a44a405bf95cff2ad385cf0",
+    "url": "/pomodoro/index.html"
+  },
+  {
+    "revision": "1a04cdd1e320074b5663",
+    "url": "/pomodoro/static/css/main.cb9e985a.chunk.css"
+  },
+  {
+    "revision": "51094f3fe8fe0955c394",
+    "url": "/pomodoro/static/js/2.51445301.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/pomodoro/static/js/2.51445301.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1a04cdd1e320074b5663",
+    "url": "/pomodoro/static/js/main.4003fb43.chunk.js"
+  },
+  {
+    "revision": "a9aee71dab78a19abc00",
+    "url": "/pomodoro/static/js/runtime-main.50bf0b17.js"
+  }
+]);
